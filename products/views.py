11e50b747d969d3b54f425c8e78faa71eb099c51
Alex <97349499+Web-energumen@users.noreply.github.com>
2024-03-30 20:58:10 +0200

    Add product function in products.views

--- products/views.py
+++ products/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
+    context = {
+        'title': 'Test title',
+        'username': 'Alex',
+    }
     return render(request, 'products/index.html')
+
+
+def products(request):
+    return render(request, 'products/products.html')
