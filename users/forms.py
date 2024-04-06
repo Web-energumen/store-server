c2b6e91cc00db07d8d073bf6f547d102216da21d
Alex <97349499+Web-energumen@users.noreply.github.com>
2024-04-06 19:27:31 +0300

    Implemented authentication and authorization mechanism and implemented a user registration mechanism

---
+++ users/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть ім\'я користувача'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть пароль'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть ім\'я'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть прізвище'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть ім\'я користувача'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть адресу ел. пошти'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Введіть пароль'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4", 'placeholder': 'Підтвердіть пароль'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
