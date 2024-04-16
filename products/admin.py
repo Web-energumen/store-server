0f3d39d33252a6cbfee9b9876ff4cce360021fb0
Alex <97349499+Web-energumen@users.noreply.github.com>
2024-04-16 13:49:58 +0300

    admin panel management

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', 'price', 'quantity', 'image', 'category')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
