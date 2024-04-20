b264f8f46e95313ab5654c62612540e4cd54d413
Alex <97349499+Web-energumen@users.noreply.github.com>
2024-04-21 00:17:29 +0300

    Created TitleMixin and added to controllers

--- products/views.py
+++ products/views.py
@@ -2,24 +2,21 @@ from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
 
 from products.models import ProductCategory, Product, Basket
+from common.views import TitleMixin
 
 
-class IndexView(TemplateView):
+class IndexView(TitleMixin, TemplateView):
     template_name = 'products/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['title'] = 'Store'
-        return context
+    title = 'Store'
 
 
-class ProductListView(ListView):
+class ProductListView(TitleMixin, ListView):
     model = Product
     template_name = 'products/products.html'
     paginate_by = 3
+    title = 'Store - Каталог'
 
     def get_queryset(self):
         queryset = super(ProductListView, self).get_queryset()
@@ -28,7 +25,6 @@ class ProductListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ProductListView, self).get_context_data()
-        context['title'] = 'Store - Каталог'
         context['categories'] = ProductCategory.objects.all()
         return context
 
--- users/views.py
+++ users/views.py
@@ -1,74 +1,39 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from products.models import Basket
+from common.views import TitleMixin
 
 
-class UserLoginView(LoginView):
+class UserLoginView(TitleMixin, LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
+    title = 'Store - Авторизація'
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
-
-    def get_context_data(self, **kwargs):
-        context = super(UserRegisterView, self).get_context_data()
-        context['title'] = 'Store - Регістрація'
-        return context
+    success_message = 'Ви успішно зареєструвалися!'
+    title = 'Store - Регістрація'
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    title = 'Особистий кабінет'
 
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
     def get_context_data(self, **kwargs):
         context = super(UserProfileView, self).get_context_data()
-        context['title'] = 'Store - Регістрація'
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
-
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Ви успішно зареєструвалися!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegisterForm()
-#     context = {
-#         'title': 'Store - Регістрація',
-#         'form': form
-#     }
-#     return render(request, 'users/registration.html', context)
-
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-#
-#     baskets = Basket.objects.filter(user=request.user)
-#     context = {
-#         'title': 'Store - Профіль',
-#         'form': form,
-#         'baskets': baskets,
-#     }
-#     return render(request, 'users/profile.html', context)
